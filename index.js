6e086a3e430f53224f4f95ab9fc1b3937fe38cdc
Kevin Kelley <kkelley622@gmail.com>
2021-12-21 17:20:07 -0600

    commit tree 9ccbf340de227a6499ce618b0393b99596d444cd

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+//defines saturdayFun function declaration as specified
+
+function saturdayFun(event = 'roller-skate') {
+    return `This Saturday, I want to ${event}!`
+}
+
+saturdayFun('bathe my dog');
+
+// defines mondayWork function expression as specified
+
+
+
+const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+
+mondayWork('work from home');
+
+// defines wrapAdjective function according to the specification
+
+function wrapAdjective(highlight = '*') {
+
+    return function (adjective = 'a hard worker') {
+       return `You are ${highlight}${adjective}${highlight}!`;
+    }
+}
+
+wrapAdjective('||')('a dedicated programmer');
